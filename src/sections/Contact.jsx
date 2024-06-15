29e0293a78061ff63e276ba6bfc97b7e8c986ac9
Harishgopal17 <harishgopal1705@gmail.com>
2024-06-15 14:23:12 +0530

    changes in profile

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -153,7 +153,7 @@ export default function Contact() {
               alt={photo.title}
               width={150}
               height={150}
-              className="rounded-full grayscale-[70%] transition-all duration-300 hover:grayscale-[0%]"
+              className="rounded-full grayscale-[70%] opacity-60 transition-all duration-300 hover:grayscale-[0%] hover:opacity-100"
             />
           </div>
           <h4 className="text-shade-green text-2xl font-semibold">
